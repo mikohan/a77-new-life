147174590450f9d8c71044a416effdd771db5723
mikohan <angara99@gmail.com>
2021-12-06 13:11:32 +0300

    Fixed cart related stuff

--- src/backend/includes/header/header.html.php
+++ src/backend/includes/header/header.html.php
@@ -78,7 +78,7 @@
             </button>
           </div>
           <div class="mobile-indicator d-none d-md-block">
-            <a href="account-login.html" class="mobile-indicator__button">
+            <a href="/account/login/" class="mobile-indicator__button">
               <span class="mobile-indicator__icon"><svg width="20" height="20">
                   <path d="M20,20h-2c0-4.4-3.6-8-8-8s-8,3.6-8,8H0c0-4.2,2.6-7.8,6.3-9.3C4.9,9.6,4,7.9,4,6c0-3.3,2.7-6,6-6s6,2.7,6,6
   c0,1.9-0.9,3.6-2.3,4.7C17.4,12.2,20,15.8,20,20z M14,6c0-2.2-1.8-4-4-4S6,3.8,6,6s1.8,4,4,4S14,8.2,14,6z" />
@@ -87,7 +87,7 @@
             </a>
           </div>
           <div class="mobile-indicator d-none d-md-block">
-            <a href="wishlist.html" class="mobile-indicator__button">
+            <a href="" class="mobile-indicator__button">
               <span class="mobile-indicator__icon">
                 <svg width="20" height="20">
                   <path d="M14,3c2.2,0,4,1.8,4,4c0,4-5.2,10-8,10S2,11,2,7c0-2.2,1.8-4,4-4c1,0,1.9,0.4,2.7,1L10,5.2L11.3,4C12.1,3.4,13,3,14,3 M14,1
@@ -97,7 +97,7 @@
             </a>
           </div>
           <div class="mobile-indicator">
-            <a href="/order/" class="mobile-indicator__button">
+            <a href="/cart/" class="mobile-indicator__button">
               <span class="mobile-indicator__icon">
                 <svg width="20" height="20">
                   <circle cx="7" cy="17" r="2" />
@@ -548,7 +548,7 @@
             </div>
             <div class="dropcart__actions">
               <a href="/cart/" class="btn btn-secondary">Перейти в Корзину</a>
-              <a href="/order/" class="btn btn-primary">Оформить</a>
+              <a href="/order/" class="btn btn-primary">Оформить Заказ</a>
             </div>
           </div>
         </div>
