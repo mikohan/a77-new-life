d677d296ffe01614ccf761c57917209bb3c9321a
mikohan <angara99@gmail.com>
2021-12-17 17:16:19 +0300

    Done product redirect

--- src/backend/pages/redirect/part/index.php
+++ src/backend/pages/redirect/part/index.php
@@ -1,20 +1,45 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 require_once __DIR__ . '/../../../lib/init.php';
 include __DIR__ . '/../data/redirect_data.php';
 
-$old_car_name = $_GET['old_car_name'];
-$old_car_id = $_GET['old_car_id'];
+$cat_number = $_GET['cat_number'];
 
-$car_slug = 'ljuboj-avtomobil';
-$new_car_slug = $ang_cars[$old_car_id] ?? false;
-if ($new_car_slug) {
-  $car_slug = $new_car_slug;
+
+function getDataFromAPI($cat_number)
+
+{
+  /**
+   * Get data from API
+   */
+
+  $server_url = PHOTO_API_URL;
+  $url = "{$server_url}/api/product/get-redirect-product-by-cat-number/{$cat_number}/";
+  //  Initiate curl
+  $ch = curl_init($url);
+  $options = array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => array('Content-type: application/json')
+  );
+  curl_setopt_array($ch, $options);
+  $result = curl_exec($ch);
+  $info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  if ($info == 200) {
+    $mydata = $result;
+  } else {
+    throw new Error("API not reacheble check it out!");
+  }
+  curl_close($ch);
+  return $mydata;
 }
-
-$conn = new Connection;
-
-$car = $conn->getCar($car_slug);
-
-$url = $u->car($car['make']['slug'], $car['slug']);
-
-header("Location: {$url}", TRUE, 301);
+// Getting product
+try {
+  $product = json_decode(getDataFromAPI($cat_number), true);
+  $url = $u->product($product['slug']);
+  header("Location: {$url}", TRUE, 301);
+} catch (Throwable $t) {
+  http_response_code(404);
+  header("Location: /404/");
+  exit();
+}
