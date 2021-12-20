82d07b6f773deeb5d2094f8df1a96f7349292b8c
mikohan <angara99@gmail.com>
2021-12-20 15:22:38 +0300

    Almost done with yandex xml

--- src/backend/pages/yandex.xml/YandexXmlModel.php
+++ src/backend/pages/yandex.xml/YandexXmlModel.php
@@ -7,7 +7,7 @@ class YandexXmlModel extends Connection
   // All Categories
   private $all_cats_url = PHOTO_API_URL .  "/api/product/get-all-categories-flat/";
   // All products
-  private $all_products_url =  PHOTO_API_URL . "/api/product/product-sitemap-list/";
+  private $all_products_url =  PHOTO_API_URL . "/api/product/jsontest-a77-yandex-markety-xml";
 
   private $host = ANG_HTTP;
 
@@ -29,97 +29,23 @@ class YandexXmlModel extends Connection
     return $mydata;
   }
 
-  private function staticPages()
-  {
-    /**
-     * Return array of static pages
-     */
-    $u = new Url;
-    $pages =  [
-      $u->home(),
-      $u->blog(),
-      $u->delivery(),
-      $u->contacts(),
-      $u->policy(),
-      $u->about(),
-      $u->garranty()
-    ];
-    return array_map(fn ($url) => $this->host . $url, $pages);
-  }
-
-  private function categoriesPages()
+  public function categoriesPages()
   {
     /**
      * Return all categories urls
      */
     $u = new Url;
     $api_data = json_decode($this->getApi($this->all_cats_url), true);
-    return array_map(fn ($item) => $this->host . $u->category($item['slug']), $api_data);
+    return  $api_data;
   }
 
-  private function carsCategoriesPages()
-  {
-    /**
-     * Getting category/model/categoryslug/ url from mysql
-     * 
-     */
-    $u = new Url;
-    $m = $this->db();
-    $q = "SELECT * FROM `ang_cars_api`";
-    $t = $m->prepare($q);
-    $t->execute();
-    $data = $t->fetchAll(PDO::FETCH_ASSOC);
-    $urls = [];
-    foreach ($data as $car) {
-      $car_data = json_decode($car['car_json'], true);
-      foreach ($car_data['categories'] as $category) {
-        $url = $this->host . $u->categoryCar($car['slug'], $category['slug']);
-        $urls[] = $url;
-      }
-    }
-    return ($urls);
-  }
-  private function blogPostsPages()
-  {
-    /**
-     * Return all articles from db
-     */
-
-    $u = new Url;
-    $m = $this->db();
-    $q = "SELECT slug FROM ang_blog_articles";
-    $t = $m->prepare($q);
-    $t->execute();
-    $data = $t->fetchAll(PDO::FETCH_ASSOC);
-    return array_map(fn ($item) => $this->host . $u->blogPost($item['slug']), $data);
-  }
-  private function productsPages()
+  public function getProducts()
   {
     /**
      * Getting products from api and return products urls
      */
     $u = new Url;
     $data = json_decode($this->getApi($this->all_products_url), true);
-    return array_map(fn ($item) => $this->host . $u->product($item['slug']), $data);
-  }
-
-
-
-
-  public function makeMeHappy()
-  {
-    /**
-     * Combine all array together and than return it
-     */
-    $return = [];
-    $static_pages = $this->staticPages();
-    $blog_post_pages = $this->blogPostsPages();
-    $categories_pages = $this->categoriesPages();
-    $cars_categories_pages = $this->carsCategoriesPages();
-    $products_pages = $this->productsPages();
-    $return = array_merge($static_pages, $blog_post_pages, $categories_pages, $cars_categories_pages, $products_pages);
-    $return = array_map(fn ($item) => "<loc>" . $item . "</loc>", $return);
-
-    return $return;
+    return $data;
   }
 }
--- src/backend/pages/yandex.xml/index.php
+++ src/backend/pages/yandex.xml/index.php
@@ -5,34 +5,115 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 include __DIR__ . '/../../lib/init.php';
 include __DIR__ . '/../yandex.xml/YandexXmlModel.php';
-require __DIR__ . '/../../../../vendor/autoload.php';
 
-$sm = new SitemapModel;
-
+$sm = new YandexXmlModel;
 
 header('Content-type: application/xml');
 header("Content-Type: text/xml; charset=utf-8");
 
 
-$cdate = date("Y-m-d H:i", time());
+$name = "Ангара";
 
-// Static pages
-$all_pages = $sm->makeMeHappy();
+# Описание магазина
 
-$loc = '';
-foreach ($all_pages as $page) {
-  $loc .= $page  . "<lastmod>$cdate</lastmod>";
+$desc = "Запчасти для грузовиков и автобусов";
+$site = COMPANY_INFO['site'];
+$cost = COMPANY_INFO['delivery_rate'];
+$curr = "RUR";
+$date_obj = new DateTime();
+$date =  $date_obj->format('Y-m-d');
+
+# Валюта
+
+// Getting categoreis from API
+$categories = $sm->categoriesPages();
+
+
+$category_string = '<categories>';
+foreach ($categories as $category) {
+  $parent_ck = $category['parent'] ?? false;
+  $parent = $parent_ck ? 'parentId="' . $category['parent'] . '"' : '';
+  $id = $category['id'];
+  $category_string .= '<category id="' . $id . '" ' . $parent . '>' . $category['name'] .  '</category>';
+}
+$category_string .= '</categories>';
+
+$products = $sm->getProducts();
+
+$offers = '';
+$i = 0;
+foreach ($products['hits']['hits'] as $product_source) {
+  $product = $product_source['_source'];
+  $id = $product['one_c_id'] ?? false;
+  if (!$id) {
+    continue;
+  }
+  // $id = $product_source['_id'];
+  $model_ck = $product['model'] ?? false;
+  $model = $model_ck ? $product['model'][0]['name'] : '';
+  $make = $model_ck ? $product['model'][0]['make']['name'] : '';
+
+  $name2 = $product['name2'] ?? '';
+  $name = $product['name'] . ' ' . $make . ' ' . $model . ' ' . $name2;
+
+  $brand = $product['brand']['name'] ?? '';
+  $url = $u->product($product['slug']);
+
+  $pictures = '';
+  foreach ($product['images'] as $img) {
+    $pictures .= "<picture>{$img['image']}</picture>";
+  }
+
+  $categoru_ck = $product['category'] ?? false;
+  $category_id = $categoru_ck ? $product['category'][0]['id'] : null;
+
+  $offers .= <<<HTML
+    <offer id="{$id}">
+      <url>{$url}}/?utm_source=market.yandex.ru</url>
+      <price>{$product['price']}</price>
+      <currencyId>{$curr}</currencyId>
+      <categoryId>{$category_id}</categoryId>
+      {$pictures}
+      <vendor>{$brand}</vendor>
+      <model>{$make} {$model}</model>
+      <vendorCode>{$product['cat_number']}</vendorCode>
+      <name>{$name}</name>
+      <description>{$name} от производителя {$brand} для автомобиля {$make} {$model} . На все запчасти есть сертификат соответсвия.</description>
+      <delivery>true</delivery>
+      <pickup>true</pickup>
+      <delivery-options>
+      <option cost="{$cost}" days="1" order-before="18" />
+      </delivery-options>
+      <pickup-options>
+      <option cost="0" days="1" />                        
+      </pickup-options>
+      <store>true</store>   
+    </offer>
+HTML;
+  if ($i == 10) {
+    break;
+  }
+  $i++;
 }
 
+$out = <<<HTML
+<?xml version="1.0" encoding="utf-8"?>
+<yml_catalog date="{$date}">
+<shop>
+<name>{$name}</name>
+<company>{$desc}</company>
+<url>{$site}</url>
+<currencies>
+    <currency id="RUR" rate="1"/>
+</currencies>
+<categories>
+  {$category_string}
+</categories>
+<offers>
+{$offers}
+</offers>
+</shop>
+</yml_catalog>
+HTML;
 
-
-
-$head = <<<EOD
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-   $loc 
-  </url>
-</urlset>
-EOD;
-echo $head;
+echo $out;
