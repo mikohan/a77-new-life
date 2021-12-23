7de5e761023038838dd336e814514d2debac4d9b
mikohan <angara99@gmail.com>
2021-12-23 09:46:44 +0300

    Added group redirect

---
+++ src/backend/pages/redirect/group/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../../../lib/init.php';
+include __DIR__ . '/../data/redirect_data.php';
+include __DIR__ . '/../data/sub.php';
+
+$car_id = $_GET['car_id'];
+$cat_id = $_GET['cat_id'];
+
+// Getting car 
+
+$car_slug = 'ljuboj-avtomobil';
+$new_car_slug = $ang_cars[$car_id] ?? false;
+if ($new_car_slug) {
+  $car_slug = $new_car_slug;
+}
+$conn = new Connection;
+// Getting car object
+$car = $conn->getCar($car_slug);
+
+// Here logic to get category slug
+// p($old_cats);
+
+$category_slug = 'zapchasti';
+$new_cat_slug = $subcats[$cat_id] ?? false;
+if ($new_cat_slug) {
+  $category_slug = $new_cat_slug;
+}
+
+$url = $u->categoryCar($car['slug'], $category_slug);
+
+header("Location: {$url}", TRUE, 301);
