3ffc25610339701bbc4d1fdb04e9bd6700bd6a88
mikohan <angara99@gmail.com>
2022-10-03 15:23:20 +0300

    Fixed order page

--- src/assets/js/order.page.js
+++ src/assets/js/order.page.js
@@ -1,91 +1,91 @@
 import { shoppingCart } from './shoppingCart';
-(function ($) {
-	var myCart = shoppingCart.listCart();
-	if (myCart.length === 0) {
-		$('#checkout-id').html(`<h2 style="margin: 0 auto !important;">Корзина пуста!</h2>`);
-	}
+(function($) {
+  var myCart = shoppingCart.listCart();
+  if (myCart.length === 0) {
+    $('#checkout-id').html(`<h2 style="margin: 0 auto !important;">Корзина пуста!</h2>`);
+  }
 
-	$('#send-order').on('click', function (e) {
-		sendOrder(e);
-	});
+  $('#send-order').on('click', function(e) {
+    sendOrder(e);
+  });
 
-	$('#pay-online').on('click', function (e) {
-		var yandexPhone = $('#yandex-phone').val();
-		if (!yandexPhone) {
-			alert('Телефон обязателен!');
-		}
-		if (yandexPhone) {
-			sendOrder(e);
-		}
-	});
+  $('#pay-online').on('click', function(e) {
+    var yandexPhone = $('#yandex-phone').val();
+    if (!yandexPhone) {
+      alert('Телефон обязателен!');
+    }
+    if (yandexPhone) {
+      sendOrder(e);
+    }
+  });
 
-	function sendOrder(event) {
-		event.preventDefault();
-		var form = [];
-		var phone = $('#checkout-phone').val();
-		var email = $('#checkout-email').val();
-		var lastname = $('#checkout-lastname').val();
-		var firstname = $('#checkout-firstname').val();
-		var city = $('#checkout-city').val();
-		var street = $('#checkout-street').val();
-		var comment = $('#checkout-comment').val();
-		var total_sum = shoppingCart.totalCart();
-		var myCart = shoppingCart.listCart();
-		var paymentMethod = 'offline';
+  function sendOrder(event) {
+    event.preventDefault();
+    var form = [];
+    var phone = $('#checkout-phone').val();
+    var email = $('#checkout-email').val();
+    var lastname = $('#checkout-lastname').val();
+    var firstname = $('#checkout-firstname').val();
+    var city = $('#checkout-city').val();
+    var street = $('#checkout-street').val();
+    var comment = $('#checkout-comment').val();
+    var total_sum = shoppingCart.totalCart();
+    var myCart = shoppingCart.listCart();
+    var paymentMethod = 'offline';
 
-		var customer = {
-			email,
-			phone,
-			lastname,
-			firstname,
-			city,
-			street,
-			comment,
-		};
-		var toThank = {
-			paymentMethod,
-			firstname,
-			city,
-			street,
-		};
+    var customer = {
+      email,
+      phone,
+      lastname,
+      firstname,
+      city,
+      street,
+      comment,
+    };
+    var toThank = {
+      paymentMethod,
+      firstname,
+      city,
+      street,
+    };
 
-		var form = {
-			customer,
-			total_sum,
-			cart: myCart,
-		};
-		if (!phone || phone == '') {
-			alert('Введите телефон ' + phone);
-			return;
-		}
-		if (!email || email == '') {
-			alert('Введите email ' + phone);
-			return;
-		}
+    var form = {
+      customer,
+      total_sum,
+      cart: myCart,
+    };
+    if (!phone || phone == '') {
+      alert('Введите телефон ' + phone);
+      return;
+    }
+    // if (!email || email == '') {
+    // 	alert('Введите email ' + phone);
+    // 	return;
+    // }
 
-		var params = '/backend/pages/order/thank.php?';
-		$.each(toThank, (key, value) => {
-			if (value) {
-				params += `${key}=${value}&`;
-			}
-		});
-		const url = params.replace(/&+$/, '');
+    var params = '/backend/pages/order/thank.php?';
+    $.each(toThank, (key, value) => {
+      if (value) {
+        params += `${key}=${value}&`;
+      }
+    });
+    const url = params.replace(/&+$/, '');
 
-		// Send emails and store order and detalis to db
-		$.post(
-			'/backend/pages/order/manage_order.php',
-			{
-				data: form,
-			},
-			function (data) {
-				if (data) {
-					// Redirect to thank page
-					window.location = url;
-				} else {
-					alert('Корзина напрочь пуста. Нечего отправлять  ☠ ');
-					return;
-				}
-			}
-		);
-	}
+    // Send emails and store order and detalis to db
+    $.post(
+      '/backend/pages/order/manage_order.php',
+      {
+        data: form,
+      },
+      function(data) {
+        if (data) {
+          // Redirect to thank page
+          window.location = url;
+        } else {
+          alert('Корзина напрочь пуста. Нечего отправлять  ☠ ');
+          return;
+        }
+      }
+    );
+  }
 })(jQuery);
--- src/templates/order.html.php
+++ src/templates/order.html.php
@@ -43,65 +43,9 @@
             <div class="col-12 mb-3">
               <!--<div class="alert alert-lg alert-primary">Уже есть аккаунт? <a href="<?= $u->login() ?>">Залогиниться</a></div>-->
             </div>
-            <div class="col-12 col-lg-6 col-xl-7">
-              <div class="card mb-lg-0">
-                <div class="card-body card-body--padding--2">
-                  <h3 class="card-title">Адрес и телефон</h3>
-                  <div class="form-group">
-                    <label for="checkout-phone">Телефон <span class="text-muted">*</span></label>
-                    <input required type="text" class="form-control" id="checkout-phone" placeholder="Телефон">
-                  </div>
-                  <div class="form-group">
-                    <label for="checkout-email">Email (Не обязательно) <span class="text-muted"></span></label>
-                    <input required type="email" class="form-control" id="checkout-email" placeholder="Емейл">
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label for="checkout-lastname">Фамилия <span class="text-muted"></span></label>
-                      <input type="text" class="form-control" id="checkout-lastname" placeholder="Фамилия">
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label for="checkout-firstname">Имя <span class="text-muted"></span></label>
-                      <input type="text" class="form-control" id="checkout-firstname" placeholder="Имя">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="checkout-city">Город</label>
-                    <input type="text" class="form-control" id="checkout-city" placeholder="Город">
-                  </div>
-                  <div class="form-group">
-                    <label for="checkout-street">Улица и Дом</label>
-                    <input type="text" class="form-control" id="checkout-street" placeholder="Улица дом корпус квартира">
-                  </div>
-                  <div class="form-group">
-                    <div class="form-check">
-                      <span class="input-check form-check-input">
-                        <span class="input-check__body">
-                          <input class="input-check__input" type="checkbox" id="checkout-create-account">
-                          <span class="input-check__box"></span>
-                          <span class="input-check__icon"><svg width="9px" height="7px">
-                              <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
-                            </svg>
-                          </span>
-                        </span>
-                      </span>
-                      <label class="form-check-label" for="checkout-create-account">Запомнить?</label>
-                    </div>
-                  </div>
-                </div>
-                <div class="card-divider"></div>
-                <div class="card-body card-body--padding--2">
-                  <h3 class="card-title">Комментарии</h3>
-                  <div class="form-group">
-                  </div>
-                  <div class="form-group">
-                    <label for="checkout-comment">Комментарии к заказу <span class="text-muted">(Не обязательно)</span></label>
-                    <textarea id="checkout-comment" class="form-control" rows="4"></textarea>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-12 col-lg-6 col-xl-5 mt-4 mt-lg-0">
+            <!--Here starts left and right column -->
+
+            <div class="col-12 col-lg-6 col-xl-7 mt-4 mt-lg-0">
               <div class="card mb-0">
                 <div class="card-body card-body--padding--2">
                   <h3 class="card-title">Ваш заказ</h3>
@@ -109,7 +53,7 @@
                     <thead class="checkout__totals-header">
                       <tr>
                         <th>SKU</th>
-                        <th>Продукт</th>
+                        <th class="order-table-widht">Продукт</th>
                         <th>Всего</th>
                       </tr>
                     </thead>
@@ -128,60 +72,7 @@
                       </tr>
                     </tfoot>
                   </table>
-                  <div class="checkout__payment-methods payment-methods">
-                    <ul class="payment-methods__list">
-                      <li class="payment-methods__item payment-methods__item--active">
-                        <label class="payment-methods__item-header">
-                          <span class="payment-methods__item-radio input-radio">
-                            <span class="input-radio__body">
-                              <input class="input-radio__input" name="checkout_payment_method" type="radio" checked>
-                              <span class="input-radio__circle"></span>
-                            </span>
-                          </span>
-                          <span class="payment-methods__item-title">Оплата при получении</span>
-                        </label>
-                        <div class="payment-methods__item-container">
-                          <div class="payment-methods__item-details text-muted">
-                            Оплатить наличными или переводом курьеру при получении
-                          </div>
-                        </div>
-                      </li>
-                      <li class="payment-methods__item">
-                        <label class="payment-methods__item-header">
-                          <span class="payment-methods__item-radio input-radio">
-                            <span class="input-radio__body">
-                              <input class="input-radio__input" name="checkout_payment_method" type="radio">
-                              <span class="input-radio__circle"></span>
-                            </span>
-                          </span>
-                          <span class="payment-methods__item-title">Оплата на сайте картой</span>
-                        </label>
-                        <div class="payment-methods__item-container">
-                          <div class="payment-methods__item-details text-muted">
-                            <form action="https://yoomoney.ru/eshop.xml" method="post">
-                              <div class="card-body card-body--padding--2">
-                                <div class="row cart-order-labels">
-                                  <div class="form-group">
-                                    <label for="checkout-summ">Сумма</label>
-                                    <input type="text" class="form-control total-cart-input" id="checkout-summ" required name="sum" value="" type="number" min="1" placeholder="Укажите сумму платежа">
-                                    <input required name="shopId" value="659633" type="hidden" />
-                                    <input required name="scid" value="1313547" type="hidden" />
-                                    <input required name="shopSuccessURL" value="<?= $call_back_url ?>" type="hidden" />
-                                  </div>
-                                  <div class="row cart-order-labels">
-                                    <div class="form-group">
-                                      <label class="control-label">Ваш Телефон</label>
-                                      <input id="yandex-phone" class="form-control" required name="customerNumber" value="" />
-                                    </div>
-                                  </div>
-                                </div>
-                                <button id="pay-online" type="submit" class="btn btn-primary btn-xl btn-block">Оплатить онлайн</button>
-                            </form>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
+                  
                   <div class="checkout__agree form-group">
                     <div class="form-check">
                       <span class="input-check form-check-input">
@@ -195,12 +86,38 @@
                         </span>
                       </span>
                       <label class="form-check-label" for="checkout-terms">
-                        Я согласен <a target="_blank" href="policy.php"> Политикой обработки персональных данных</a>
+                        Я согласен c Политикой обработки персональных данных
                       </label>
                     </div>
                   </div>
-                  <button id="send-order" type="submit" class="btn btn-primary btn-xl btn-block">Отправить заказ</button>
+                  
                 </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 col-xl-5">
+              <div class="card mb-lg-0">
+                <div class="card-body card-body--padding--2">
+                  <h3 class="card-title">Быстрое оформление заказа</h3>
+                  <div class="form-group">
+                    <label for="checkout-phone">Телефон <span class="text-muted">*</span></label>
+                    <input required type="text" class="form-control" id="checkout-phone" placeholder="Телефон">
+                  </div>
+                  
+                  <div class="form-row">
+                    <div class="form-group col-md-12">
+                      <label for="checkout-firstname">Имя <span class="text-muted"></span></label>
+                      <input type="text" class="form-control" id="checkout-firstname" placeholder="Имя">
+                    </div>
+                  </div>
+                  <div class="text-muted p-1">
+                   Оформите заказ и товар будет зарезервирован за Вами.
+                  </div>
+                  <button id="send-order" type="submit" class="btn btn-primary btn-xl btn-block">Оформить заказ</button>
+                  <div class="text-muted p-1">                  
+                   Менеджер свяжется с Вами для уточнения способа доставки и оплаты.
+                  </div>
+                </div>                
               </div>
             </div>
           </div>
