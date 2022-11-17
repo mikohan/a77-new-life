16970b366fa01d93d44d3400a0e143db553c7e05
mikohan <angara99@gmail.com>
2022-11-17 10:05:11 +0300

    Added settimeout to ym

--- src/backend/includes/header/ga.php
+++ src/backend/includes/header/ga.php
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-4786389-1"></script>
+<script defer async src="https://www.googletagmanager.com/gtag/js?id=UA-4786389-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
 
@@ -15,27 +15,29 @@
 
 <!-- Yandex.Metrika counter -->
 <script>
-  (function(m, e, t, r, i, k, a) {
-    m[i] = m[i] || function() {
-      (m[i].a = m[i].a || []).push(arguments)
-    };
-    var z = null;
-    m[i].l = 1 * new Date();
-    for (var j = 0; j < document.scripts.length; j++) {
-      if (document.scripts[j].src === r) {
-        return;
-      }
-    }
-    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-  })
-  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  let fired = false;
 
-  ym(20154349, "init", {
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    webvisor: true
-  });
+  window.addEventListener('scroll', ()=> {
+    if(fired === false){
+      fired === true;
+      setTimeout(()=> {
+         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(20154349, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+            });
+
+      }, 1000)
+    }
+  })
+
 </script>
 <!-- /Yandex.Metrika counter -->
 <meta name="yandex-verification" content="bc25c21b07b16ef5" />
